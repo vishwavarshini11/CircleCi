c0a02fd9ec6230d6b966e0611fb98c531b3ffb0c
vishwavarshini11 <vishwavarshininadar2003@gmail.com>
2025-01-28 09:05:44 +0530

    Update Main1.py

--- Main1.py
+++ Main1.py
@@ -1,6 +1,7 @@
-def Add(a,b):
-  return a=b
+def Add(a, b):
+    return a + b  
 def Display():
-  print("Hello")
-if __name__=='__main__':
-  Display()
+    print("Hello")
+
+if __name__ == '__main__':
+    Display()  
