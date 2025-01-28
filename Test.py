979726a21a1bf3ffa757e6598c84a2316450908f
vishwavarshini11 <vishwavarshininadar2003@gmail.com>
2025-01-28 09:06:13 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,8 @@
 from Main1 import Add
+
 def TestAdd():
-  assert Add(3,4)==7
-  print("Add function works correctly")
-if __name__ =='__main__':
-  TestAdd()
+    assert Add(3, 4) == 7
+    print("Add function works correctly")
+
+if __name__ == '__main__':
+    TestAdd()  # Fixed the invalid (clear error) part
