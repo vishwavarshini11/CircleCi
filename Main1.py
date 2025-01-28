957053958649fef4d0f5bad6e6281a403e825133
vishwavarshini11 <vishwavarshininadar2003@gmail.com>
2025-01-28 08:54:45 +0530

    Add files via upload

---
+++ Main1.py
@@ -0,0 +1,6 @@
+def Add(a,b):
+  return a=b
+  def Display():
+    print("Hello")
+  if __name__=='__main__':
+    Display()
---
+++ Test.py
@@ -0,0 +1,5 @@
+from Main1 import Add
+deftestAdd(3,4)==7
+print("Add function works correctly")
+if __name__=='__main__':
+  TestAdd()
